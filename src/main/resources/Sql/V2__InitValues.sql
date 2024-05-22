8ab4d2e98ce607ee4ff0655903cbe8d131205ca7
1 <alex130718@mail.ru>
2024-05-22 15:05:30 +0300

    Clean some code

---
+++ src/main/resources/Sql/V2__InitValues.sql
@@ -0,0 +1,9 @@
+INSERT INTO Users (login, password, fio, telephone, email, balance, first_deposit, birth) VALUES ('user9', 'pass5', 'John MacDonald', '79005001215', 'a@mail.ru',300,300,'2000-01-01');
+
+INSERT INTO Users (login, password, fio, telephone, email, balance, first_deposit, birth) VALUES ('user11', 'pass5', 'Ronald Donald', '79505207275', 'b@mail.ru',500,500,'2005-01-01');
+
+INSERT INTO Users (login, password, fio, telephone, email, balance, first_deposit, birth) VALUES ('user20', 'pass5', 'Aluma Luna', '79595207979', 'c@mail.ru',700,700,'1990-01-01');
+
+INSERT INTO Users (login, password, fio, telephone, email, balance, first_deposit, birth) VALUES ('user50', 'pass5', 'Long Bucket', '79605267976', 'd@mail.ru',1000,1000,'1995-01-01');
+
+INSERT INTO Users (login, password, fio, telephone, email, balance, first_deposit, birth) VALUES ('user70', 'pass5', 'Short Bucket', '79615267111', 'e@mail.ru',5000,5000,'1997-01-01');
